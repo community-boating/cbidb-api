2440e76a53323f17a945b7e67850e86d981bd74b
Jonathan <coleji@bc.edu>
2017-08-22 23:41:38 -0400

    works with mysql and oracle

---
+++ app/Services/MysqlConnectionPoolConstructor.java
@@ -0,0 +1,35 @@
+package Services;
+
+import CbiUtil.PropertiesWrapper;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+public class MysqlConnectionPoolConstructor implements ConnectionPoolConstructor {
+    public ComboPooledDataSource getPool() {
+        ComboPooledDataSource cpds = new ComboPooledDataSource();
+        try {
+            //mysql -u root -p -e "GRANT ALL PRIVILEGES ON $schema.* TO $user@localhost IDENTIFIED BY '$db_pass'"
+            PropertiesWrapper props = new PropertiesWrapper(
+                    "conf/private/db-credentials",
+                    new String[] {"username", "password", "host", "port"}
+            );
+            String connectionString = "jdbc:mysql://" + props.getProperty("host") + ":" + props.getProperty("port")
+                    + "/" + props.getProperty("schema")
+                    + "?user=" + props.getProperty("username")
+                    + "&password=" + props.getProperty("password")
+                    + "&useSSL=false";
+
+
+            cpds.setDriverClass( "com.mysql.jdbc.Driver" ); //loads the jdbc driver
+            cpds.setJdbcUrl(connectionString );
+
+
+            // the settings below are optional -- c3p0 can work with defaults
+            cpds.setMinPoolSize(5);
+            cpds.setAcquireIncrement(5);
+            cpds.setMaxPoolSize(20);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return cpds;
+    }
+}
--- app/Services/OracleBroker.scala
+++ app/Services/OracleBroker.scala
@@ -4,7 +4,7 @@ import javax.inject.Inject
 
 import play.api.inject.ApplicationLifecycle
 
-class OracleBroker @Inject() (lifecycle: ApplicationLifecycle, cp: ConnectionPoolConstructor)
+class OracleBroker @Inject() (lifecycle: ApplicationLifecycle, cp: OracleConnectionPoolConstructor)
   extends RelationalBroker(lifecycle: ApplicationLifecycle, cp: ConnectionPoolConstructor) {
 
 }
