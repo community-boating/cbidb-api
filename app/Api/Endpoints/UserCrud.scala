d0c42ad7e6363c7f13c6fecb2405d63534cde2ce
Jonathan <coleji@bc.edu>
2017-09-16 16:45:01 -0400

    playing around with posts

---
+++ app/Api/Endpoints/UserCrud.scala
@@ -0,0 +1,29 @@
+package Api.Endpoints
+
+import javax.inject.Inject
+
+import Entities.User
+import Services.{CacheBroker, PersistenceBroker}
+import play.api.inject.ApplicationLifecycle
+import play.api.mvc.{Action, Controller}
+
+import scala.concurrent.ExecutionContext
+
+class UserCrud @Inject() (lifecycle: ApplicationLifecycle, cb: CacheBroker, pb: PersistenceBroker)(implicit exec: ExecutionContext) extends Controller {
+  def post = Action { request => {
+    val data = request.body.asFormUrlEncoded
+    data match {
+      case None => println("no body")
+      case Some(v) => {
+        val userFields: Set[String] = User.fieldList.map(_.getFieldName).toSet
+        val reqFields: Set[String] = v.keySet
+        val unspecifiedFields: Set[String] = userFields -- reqFields
+        println(userFields)
+        println(reqFields)
+        println(unspecifiedFields)
+      }
+      case _ => println("wasnt an object")
+    }
+    Ok("yo\n")
+  }}
+}
