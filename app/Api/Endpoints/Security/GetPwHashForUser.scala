14398902b50affd6db88806c122753beaa319059
Jonathan <coleji@bc.edu>
2018-11-24 20:07:55 -0500

    support for member login

--- app/Api/Endpoints/Security/GetPwHashForUser.scala
+++ app/Api/Endpoints/Security/GetPwHashForUser.scala
@@ -1,6 +1,6 @@
 package Api.Endpoints.Security
 
-import Services.Authentication.StaffUserType
+import Services.Authentication.{MemberUserType, StaffUserType, UserType}
 import Services.PermissionsAuthority
 import javax.inject.Inject
 import play.api.mvc.{Action, AnyContent, Controller}
@@ -8,12 +8,36 @@ import play.api.mvc.{Action, AnyContent, Controller}
 import scala.concurrent.{ExecutionContext, Future}
 
 class GetPwHashForUser @Inject()(implicit exec: ExecutionContext) extends Controller {
-  def get(userName: String): Action[AnyContent] = Action.async {request => Future {
+  def get(userName: String, userType: String = "staff"): Action[AnyContent] = Action.async {request => Future {
+    println("userType is " + userType)
+    println("username is " + userName)
+    val userTypeObj: Option[UserType] = userType match {
+      case "staff" => {
+        println("looking up staff")
+        Some(StaffUserType)
+      }
+      case "member" => {
+        println("looking up member")
+        Some(MemberUserType)
+      }
+      case _ => None
+    }
     println("headers: " + request.headers)
-    PermissionsAuthority.getPwHashForUser(request, userName, StaffUserType) match {
-      case None => Ok("NO DATA")
-      // Int is the hashing scheme ID, string is the hash itself
-      case Some(t: (Int, String)) => Ok(t._1 + "," + t._2)
+    if (userTypeObj.isEmpty) Ok("BAD USER TYPE")
+    else {
+      try {
+        PermissionsAuthority.getPwHashForUser(request, userName, userTypeObj.get) match {
+          case None => Ok("NO DATA")
+          // Int is the hashing scheme ID, string is the hash itself
+          case Some(t: (Int, String)) => Ok(t._1 + "," + t._2)
+        }
+      } catch {
+        case e: Exception => {
+          println(e)
+          Ok("NO DATA")
+        }
+      }
+
     }
   }}
 }
--- app/Services/Authentication/MemberUserType.scala
+++ app/Services/Authentication/MemberUserType.scala
@@ -1,6 +1,9 @@
 package Services.Authentication
 
+import java.sql.ResultSet
+
 import CbiUtil.ParsedRequest
+import IO.PreparedQueries.PreparedQueryForSelect
 import Services._
 import Storable.{EntityVisibility, StorableClass, StorableObject}
 
@@ -12,7 +15,21 @@ object MemberUserType extends UserType {
     requiredUserName: Option[String]
   ): Option[String] = if (currentAuthentication.userType == RootUserType) Some(RootUserType.uniqueUserName) else None
 
-  def getPwHashForUser(userName: String, rootPB: PersistenceBroker): Option[(Int, String)] = None
+  def getPwHashForUser(userName: String, rootPB: PersistenceBroker): Option[(Int, String)] = {
+    case class Result (userName: String, pwHash: String)
+    val hq = new PreparedQueryForSelect[Result](allowedUserTypes = Set(BouncerUserType)) {
+      override def mapResultSetRowToCaseObject(rs: ResultSet): Result = Result(rs.getString(1), rs.getString(2))
+
+      override def getQuery: String = "select email, pw_hash from persons where pw_hash is not null and lower(email) = ?"
+
+      override val params: List[String] = List(userName.toLowerCase)
+    }
+
+    val users = rootPB.executePreparedQueryForSelect(hq)
+
+    if (users.length == 1) Some(1, users.head.pwHash)
+    else None
+  }
 
   def getEntityVisibility(obj: StorableObject[_ <: StorableClass]): EntityVisibility = EntityVisibility.ZERO_VISIBILITY
 }
