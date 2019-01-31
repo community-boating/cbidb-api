f22abdc418bf1464ee934ed93701ff68134a0f9a
Jonathan <coleji@bc.edu>
2019-01-31 16:05:54 -0500

    expanded welcome package result

--- app/Api/Endpoints/Member/WelcomePackage.scala
+++ app/Api/Endpoints/Member/WelcomePackage.scala
@@ -2,10 +2,11 @@ package Api.Endpoints.Member
 
 import Api.AuthenticatedRequest
 import CbiUtil.ParsedRequest
-import IO.PreparedQueries.Member.WelcomePackageQuery
+import IO.PreparedQueries.Member.{GetChildDataQuery, GetChildDataQueryResult, IdentifyMemberQuery}
 import Services.Authentication.MemberUserType
 import Services.PermissionsAuthority
 import javax.inject.Inject
+import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent}
 
 import scala.concurrent.{ExecutionContext, Future}
@@ -18,9 +19,27 @@ class WelcomePackage @Inject() (implicit val exec: ExecutionContext) extends Aut
     else {
       val rc = maybeRC.get
       val pb = rc.pb
-      val queryResult = pb.executePreparedQueryForSelect(new WelcomePackageQuery(rc.auth.userName)).head
-      val personId: Int = queryResult.personId
-      Future{Ok("{\"data\": {\"personId\": " + personId + ", \"userName\": \"" + rc.auth.userName + "\"}}")}
+
+      val identifyMemberResult = pb.executePreparedQueryForSelect(new IdentifyMemberQuery(rc.auth.userName)).head
+      val personId: Int = identifyMemberResult.personId
+
+      val childData = pb.executePreparedQueryForSelect(new GetChildDataQuery(personId))
+      println(Json.toJson(childData))
+
+      val result = WelcomePackageResult(personId, rc.auth.userName, childData)
+      implicit val format = WelcomePackageResult.format
+
+      Future{Ok("{\"data\": " + Json.toJson(result) + "}")}
     }
   }}
+
+  case class WelcomePackageResult (
+    personId: Int,
+    userName: String,
+    children: List[GetChildDataQueryResult]
+  )
+
+  object WelcomePackageResult {
+    implicit val format = Json.format[WelcomePackageResult]
+  }
 }
--- app/IO/PreparedQueries/Member/IdentifyMemberQuery.scala
+++ app/IO/PreparedQueries/Member/IdentifyMemberQuery.scala
@@ -6,15 +6,16 @@ import IO.PreparedQueries.PreparedQueryForSelect
 import Services.Authentication.MemberUserType
 
 // TODO: replace with entity-based arch
-class WelcomePackageQuery(userName: String) extends PreparedQueryForSelect[WelcomePackageResult](allowedUserTypes = Set(MemberUserType)) {
-  override def mapResultSetRowToCaseObject(rs: ResultSet): WelcomePackageResult = WelcomePackageResult(rs.getString(1), rs.getInt(2))
+class IdentifyMemberQuery(userName: String) extends PreparedQueryForSelect[IdentifyMemberQueryResult](allowedUserTypes = Set(MemberUserType)) {
+  override def mapResultSetRowToCaseObject(rs: ResultSet): IdentifyMemberQueryResult =
+    IdentifyMemberQueryResult(rs.getString(1), rs.getInt(2))
 
   override def getQuery: String = "select email, person_id from persons where pw_hash is not null and lower(email) = ?"
 
   override val params: List[String] = List(userName.toLowerCase)
 }
 
-case class WelcomePackageResult(
+case class IdentifyMemberQueryResult(
   email: String,
   personId: Int
 )
