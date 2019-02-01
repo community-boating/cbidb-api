e58fd2cbe4640154ef97bac8f659e35fe13447ec
Jonathan <coleji@bc.edu>
2019-01-31 21:35:16 -0500

    match param name with view

--- app/Api/Endpoints/Member/WelcomePackage.scala
+++ app/Api/Endpoints/Member/WelcomePackage.scala
@@ -1,7 +1,7 @@
 package Api.Endpoints.Member
 
 import Api.AuthenticatedRequest
-import CbiUtil.ParsedRequest
+import CbiUtil.{ParsedRequest, Profiler}
 import IO.PreparedQueries.Member.{GetChildDataQuery, GetChildDataQueryResult, IdentifyMemberQuery}
 import Services.Authentication.MemberUserType
 import Services.PermissionsAuthority
@@ -13,28 +13,30 @@ import scala.concurrent.{ExecutionContext, Future}
 
 class WelcomePackage @Inject() (implicit val exec: ExecutionContext) extends AuthenticatedRequest {
   def get(): Action[AnyContent] = Action.async {req => {
+    val profiler = new Profiler
     val logger = PermissionsAuthority.logger
     val maybeRC = getRCOption(MemberUserType, ParsedRequest(req))
     if (maybeRC.isEmpty) Future {Ok("{\"error\": \"Unauthorized\"}") }
     else {
       val rc = maybeRC.get
       val pb = rc.pb
-
+      profiler.lap("about to do first query")
       val identifyMemberResult = pb.executePreparedQueryForSelect(new IdentifyMemberQuery(rc.auth.userName)).head
       val personId: Int = identifyMemberResult.personId
-
+      profiler.lap("got person id")
       val childData = pb.executePreparedQueryForSelect(new GetChildDataQuery(personId))
       println(Json.toJson(childData))
+      profiler.lap("got child data")
 
       val result = WelcomePackageResult(personId, rc.auth.userName, childData)
       implicit val format = WelcomePackageResult.format
-
+      profiler.lap("finishing welcome pkg")
       Future{Ok("{\"data\": " + Json.toJson(result) + "}")}
     }
   }}
 
   case class WelcomePackageResult (
-    personId: Int,
+    parentPersonId: Int,
     userName: String,
     children: List[GetChildDataQueryResult]
   )
