89d66b6f8fe5a7037e2d435e98d1c8c04b239afa
Jonathan <coleji@bc.edu>
2019-05-19 18:41:12 -0400

    survey endpoint

--- app/CbiUtil/GetSQLLiteral.scala
+++ app/CbiUtil/GetSQLLiteral.scala
@@ -4,12 +4,9 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 object GetSQLLiteral {
-  def apply(s: String): String = "'" + s + "'"
-  def apply(s: Option[String]): String = s match { case Some(x) => apply(x); case None => "null" }
-  def apply(i: Int): String = i.toString
-  def apply(b: Boolean): String = if (b) "'Y'" else "'N'"
-  def apply(zdt: ZonedDateTime, truncateToDay: Boolean = false): String = {
-    if (truncateToDay) "TO_DATE('" + zdt.format(DateTimeFormatter.ofPattern("MM/dd/yyyy")) + "', 'MM/DD/YYYY')"
-    else "TO_DATE('" + zdt.format(DateTimeFormatter.ofPattern("MM/dd/yyyy  HH:mm:ss")) + "', 'MM/DD/YYYY HH24:MI:SS')"
-  }
+  def apply(s: String): String = "'" + GetSQLLiteralPrepared(s) + "'"
+  def apply(s: Option[String]): String = GetSQLLiteralPrepared(s)
+  def apply(i: Int): String = GetSQLLiteralPrepared(i)
+  def apply(b: Boolean): String = "'" + GetSQLLiteralPrepared(b) + "'"
+  def apply(zdt: ZonedDateTime, truncateToDay: Boolean = false): String = GetSQLLiteralPrepared(zdt, truncateToDay)
 }
