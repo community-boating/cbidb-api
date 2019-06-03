213c60f2f6f10395515c93c12c7392accbcd60b8
Jonathan Cole <coleji@bc.edu>
2019-06-02 20:12:01 -0400

    jp roster pdf: split by section

--- app/PDFBox/Reports/JpClassRoster/Model/JpClassInstanceData.scala
+++ app/PDFBox/Reports/JpClassRoster/Model/JpClassInstanceData.scala
@@ -3,8 +3,8 @@ package PDFBox.Reports.JpClassRoster.Model
 import java.time.ZonedDateTime
 
 case class JpClassInstanceData(
-									  instanceId: Int,
-									  typeName: String,
-									  numberSessions: Int,
-									  firstSessionTime: ZonedDateTime
-							  )
+	instanceId: Int,
+	typeName: String,
+	numberSessions: Int,
+	firstSessionTime: ZonedDateTime
+)
--- app/PDFBox/Reports/JpClassRoster/Model/JpRosterData.scala
+++ app/PDFBox/Reports/JpClassRoster/Model/JpRosterData.scala
@@ -4,16 +4,17 @@ import CbiUtil.Initializable
 import PDFBox.Abstract.RowData
 
 class JpRosterData(
-						  val personId: Int,
-						  val firstName: String,
-						  val lastName: String,
-						  val isWaitList: Boolean,
-						  val allergies: Option[String],
-						  val medications: Option[String],
-						  val specialNeeds: Option[String],
-						  val signupNote: Option[String],
-						  val numberSessions: Initializable[Int]
-				  ) extends RowData {
+	val personId: Int,
+	val firstName: String,
+	val lastName: String,
+	val isWaitList: Boolean,
+	val allergies: Option[String],
+	val medications: Option[String],
+	val specialNeeds: Option[String],
+	val signupNote: Option[String],
+	val numberSessions: Initializable[Int],
+	val sectionName: Option[String]
+) extends RowData {
 	lazy val cellValues = {
 		List(
 			lastName + ", " + firstName
