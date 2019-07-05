70f21eef27a7663042b9fab60b16b8cab580d234
Jonathan <coleji@bc.edu>
2019-07-05 16:32:19 -0400

    log errors while trying to get a DB connection or use it

--- app/Services/ConnectionPoolWrapper.scala
+++ app/Services/ConnectionPoolWrapper.scala
@@ -7,12 +7,18 @@ import com.zaxxer.hikari.HikariDataSource
 
 class ConnectionPoolWrapper(private val source: HikariDataSource) {
 	def withConnection[T](block: Connection => T): T = {
-		val c: Connection = source.getConnection()
+		var c: Connection = null
 		try {
+			c = source.getConnection()
 			println("starting block")
 			val ret = block(c)
 			println("finished block")
 			ret
+		} catch {
+			case e: Throwable => {
+				PermissionsAuthority.logger.error("Error using a DB connection: ", e)
+				throw e
+			}
 		} finally {
 			println("about to close connection")
 			c.close()
