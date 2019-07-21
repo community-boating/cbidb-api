8213023570786c00cd7c128d0f509a9e5ae086b6
Jonathan <coleji@bc.edu>
2019-07-21 17:25:21 -0400

    add is-logged-in-as-member

---
+++ app/Api/Endpoints/IsLoggedInAsMember.scala
@@ -0,0 +1,43 @@
+package Api.Endpoints
+
+import CbiUtil.ParsedRequest
+import Services.Authentication.StaffUserType
+import Services.PermissionsAuthority
+import Services.PermissionsAuthority.UnauthorizedAccessException
+import javax.inject.Inject
+import play.api.libs.json.{JsArray, JsObject, JsString}
+import play.api.mvc.{Action, AnyContent, Controller}
+
+import scala.concurrent.{ExecutionContext, Future}
+
+class IsLoggedInAsMember @Inject()(implicit exec: ExecutionContext) extends Controller {
+	def get(): Action[AnyContent] = Action.async { request =>
+		try {
+			val authResult = PermissionsAuthority.getRequestCacheMember(None, ParsedRequest(request))
+			authResult._2 match {
+				case Some(rc) => Future {
+					Ok(JsObject(Map("value" -> JsString(rc.auth.userName))))
+				}
+				case None => Future {
+					Ok(JsObject(Map("error" -> JsObject(Map(
+						"code" -> JsString("unauthorized"),
+						"message" -> JsString("Unauthorized")
+					)))))
+				}
+			}
+		} catch {
+			case _: UnauthorizedAccessException => Future {
+				Ok(JsObject(Map("error" -> JsObject(Map(
+					"code" -> JsString("unauthorized"),
+					"message" -> JsString("Unauthorized")
+				)))))
+			}
+			case _: Throwable => Future {
+				Ok(JsObject(Map("error" -> JsObject(Map(
+					"code" -> JsString("internal_error"),
+					"message" -> JsString("Internal Error")
+				)))))
+			}
+		}
+	}
+}
