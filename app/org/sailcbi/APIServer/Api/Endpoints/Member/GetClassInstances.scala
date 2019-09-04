219bdbb87d4ce4463df5b5362e59b265246b9371
Jonathan Cole <coleji@bc.edu>
2019-09-03 20:25:25 -0400

    add public side to get jp class instances api

--- app/org/sailcbi/APIServer/Api/Endpoints/Member/GetClassInstances.scala
+++ app/org/sailcbi/APIServer/Api/Endpoints/Member/GetClassInstances.scala
@@ -4,6 +4,7 @@ import javax.inject.Inject
 import org.sailcbi.APIServer.Api.AuthenticatedRequest
 import org.sailcbi.APIServer.CbiUtil.ParsedRequest
 import org.sailcbi.APIServer.IO.PreparedQueries.Member.GetClassInstancesQuery
+import org.sailcbi.APIServer.Services.Authentication.PublicUserType
 import org.sailcbi.APIServer.Services.PermissionsAuthority
 import play.api.libs.json.Json
 import play.api.mvc.{Action, AnyContent}
@@ -11,7 +12,7 @@ import play.api.mvc.{Action, AnyContent}
 import scala.concurrent.{ExecutionContext, Future}
 
 class GetClassInstances @Inject()(implicit val exec: ExecutionContext) extends AuthenticatedRequest {
-	def get(typeId: Int, juniorId: Int)(implicit PA: PermissionsAuthority): Action[AnyContent] = Action.async(req => {
+	def junior(typeId: Int, juniorId: Int)(implicit PA: PermissionsAuthority): Action[AnyContent] = Action.async(req => {
 		val parsedRequest = ParsedRequest(req)
 		val maybeRC = PA.getRequestCacheMemberWithJuniorId(None, parsedRequest, juniorId)._2
 		if (maybeRC.isEmpty) Future {
@@ -20,6 +21,18 @@ class GetClassInstances @Inject()(implicit val exec: ExecutionContext) extends A
 			val rc = maybeRC.get
 			val pb = rc.pb
 			val queryResult = pb.executePreparedQueryForSelect(GetClassInstancesQuery.byJunior(None, typeId, juniorId)).toArray
+			Future(Ok(Json.toJson(queryResult)))
+		}
+	})
+	def public(typeId: Int)(implicit PA: PermissionsAuthority): Action[AnyContent] = Action.async(req => {
+		val parsedRequest = ParsedRequest(req)
+		val maybeRC = PA.getRequestCache(PublicUserType, None, parsedRequest)._2
+		if (maybeRC.isEmpty) Future {
+			Ok("{\"error\": \"Unauthorized\"}")
+		} else {
+			val rc = maybeRC.get
+			val pb = rc.pb
+			val queryResult = pb.executePreparedQueryForSelect(GetClassInstancesQuery.public(None, typeId)).toArray
 			Future(Ok(Json.toJson(queryResult)))
 		}
 	})
