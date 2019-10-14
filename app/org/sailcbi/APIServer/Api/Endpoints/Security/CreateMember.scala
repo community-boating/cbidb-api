5ca458ef1a4c4a82e8e9a4dfb739a52b576d4008
Jonathan <coleji@bc.edu>
2019-10-14 15:31:31 -0400

    create parent: first/last/email not null

--- app/org/sailcbi/APIServer/Api/Endpoints/Security/CreateMember.scala
+++ app/org/sailcbi/APIServer/Api/Endpoints/Security/CreateMember.scala
@@ -8,7 +8,7 @@ import org.sailcbi.APIServer.IO.Junior.JPPortal
 import org.sailcbi.APIServer.Services.Authentication.ProtoPersonUserType
 import org.sailcbi.APIServer.Services.PermissionsAuthority.UnauthorizedAccessException
 import org.sailcbi.APIServer.Services.{PermissionsAuthority, PersistenceBroker, RequestCache}
-import play.api.libs.json.{JsNumber, JsObject, JsValue}
+import play.api.libs.json.{JsNumber, JsObject, JsValue, Json}
 import play.api.mvc.{Action, Controller}
 
 import scala.concurrent.ExecutionContext
@@ -16,11 +16,29 @@ import scala.concurrent.ExecutionContext
 class CreateMember @Inject()(implicit exec: ExecutionContext) extends Controller {
 	def post()(implicit PA: PermissionsAuthority) = Action { request =>
 		try {
-//			val logger = PA.logger
-//			val parsedRequest = ParsedRequest(request)
-//			val rc: RequestCache = PA.getRequestCache(ProtoPersonUserType, None, parsedRequest)._2.get
-//			val data = request.body.asJson
-			Ok("Hi from api")
+			val parsedRequest = ParsedRequest(request)
+			println(parsedRequest)
+			parsedRequest.postJSON match {
+				case Some(v: JsValue) => {
+					val cms = CreateMemberShape.apply(v)
+					val protoPersonCookieValMaybe = parsedRequest.cookies.find(_.name == ProtoPersonUserType.COOKIE_NAME).map(_.value)
+					println(cms)
+					println(protoPersonCookieValMaybe)
+					createMember(
+						firstName = cms.firstName,
+						lastName = cms.lastName,
+						username = cms.username,
+						pwHash = cms.pwHash,
+						protoPersonValue = protoPersonCookieValMaybe
+					) match {
+						case Left(v: ValidationError) => Ok(v.toResultError().asJsObject())
+						case Right(id: Int) => Ok(new JsObject(Map(
+							"personId" -> JsNumber(id)
+						)))
+					}
+				}
+				case None => Ok("Internal Error")
+			}
 		} catch {
 			case _: UnauthorizedAccessException => Ok("Access Denied")
 			case e: Throwable => {
@@ -28,6 +46,38 @@ class CreateMember @Inject()(implicit exec: ExecutionContext) extends Controller
 				e.printStackTrace()
 				Ok("Internal Error")
 			}
+		}
+	}
+
+	case class CreateMemberShape(
+		username: String,
+		firstName: String,
+		lastName: String,
+		pwHash: String
+	)
+
+	object CreateMemberShape {
+		implicit val format = Json.format[CreateMemberShape]
+
+		def apply(v: JsValue): CreateMemberShape = v.as[CreateMemberShape]
+	}
+
+	def createMember(
+		firstName: String,
+		lastName: String,
+		username: String,
+		pwHash: String,
+		protoPersonValue: Option[String]
+	): Either[ValidationError, Int] = {
+		val nothingBlank = List(
+			ValidationError.inline(firstName)(s => s != null && s.length > 0, "First Name must be specified."),
+			ValidationError.inline(lastName)(s => s != null && s.length > 0, "Last Name must be specified."),
+			ValidationError.inline(username)(s => s != null && s.length > 0, "Email must be specified.")
+		)
+
+		ValidationError.combine(nothingBlank.filter(_.isDefined).map(_.orNull)) match {
+			case Some(v) => Left(v)
+			case None => Right(-1)
 		}
 	}
 }
--- app/org/sailcbi/APIServer/CbiUtil/ParsedRequest.scala
+++ app/org/sailcbi/APIServer/CbiUtil/ParsedRequest.scala
@@ -1,5 +1,6 @@
 package org.sailcbi.APIServer.CbiUtil
 
+import play.api.libs.json.JsValue
 import play.api.mvc.{AnyContent, Cookies, Headers, Request}
 
 case class ParsedRequest(
@@ -8,7 +9,8 @@ case class ParsedRequest(
 	path: String,
 	method: String,
 	remoteAddress: String,
-	postParams: Map[String, String]
+	postParams: Map[String, String],
+	postJSON: Option[JsValue]
 ) {
 	def addHeader(h: (String, String)): ParsedRequest = ParsedRequest(
 		headers.add(h),
@@ -16,7 +18,8 @@ case class ParsedRequest(
 		path,
 		method,
 		remoteAddress,
-		postParams
+		postParams,
+		postJSON
 	)
 }
 
@@ -32,7 +35,8 @@ object ParsedRequest {
 		path = request.path,
 		method = request.method,
 		remoteAddress = request.remoteAddress,
-		postParams = getPostParams(request)
+		postParams = getPostParams(request),
+		postJSON = request.body.asJson
 	)
 
 	private def getPostParams(request: Request[AnyContent]): Map[String, String] = {
