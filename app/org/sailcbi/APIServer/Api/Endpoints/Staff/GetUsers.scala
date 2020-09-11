d900dc0ff8deb5b907bc0bb01dd3201058f540ed
Jonathan Cole <coleji@bc.edu>
2020-09-10 23:16:41 -0400

    add getUser singular endpoint

--- app/org/sailcbi/APIServer/Api/Endpoints/Staff/GetUsers.scala
+++ app/org/sailcbi/APIServer/Api/Endpoints/Staff/GetUsers.scala
@@ -4,7 +4,8 @@ import javax.inject.Inject
 import org.sailcbi.APIServer.CbiUtil.ParsedRequest
 import org.sailcbi.APIServer.Entities.EntityDefinitions.{JpClassType, User}
 import org.sailcbi.APIServer.Services.Authentication.StaffUserType
-import org.sailcbi.APIServer.Services.{PermissionsAuthority, RequestCache}
+import org.sailcbi.APIServer.Services.{PermissionsAuthority, PersistenceBroker, RequestCache}
+import org.sailcbi.APIServer.Storable.Filter
 import org.sailcbi.APIServer.Storable.StorableQuery.{QueryBuilder, TableAlias}
 import play.api.libs.json.{JsValue, Json}
 import play.api.mvc.{Action, AnyContent, InjectedController}
@@ -12,41 +13,59 @@ import play.api.mvc.{Action, AnyContent, InjectedController}
 import scala.concurrent.{ExecutionContext, Future}
 
 class GetUsers @Inject()(implicit val exec: ExecutionContext) extends InjectedController {
-	def get()(implicit PA: PermissionsAuthority): Action[AnyContent] = Action.async(req => {
-		val logger = PA.logger
-		PA.withRequestCache(StaffUserType, None, ParsedRequest(req), theRC => {
-			implicit val rc: RequestCache = theRC
-			val pb = rc.pb
-
-			val users = {
-				val users = TableAlias(User)
-				val qb = QueryBuilder
-					.from(users)
-					.select(List(
-						User.fields.userId,
-						User.fields.userName,
-						User.fields.nameLast,
-						User.fields.nameFirst,
-						User.fields.email,
-						User.fields.locked,
-						User.fields.pwChangeRequired,
-						User.fields.active,
-						User.fields.hideFromClose
-					).map(_.alias(users)))
-				pb.executeQueryBuilder(qb).map(User.construct)
+	private def get(userId: Option[Int], theRC: RequestCache): List[UserShape] = {
+		implicit val rc: RequestCache = theRC
+		val pb = rc.pb
+		val users = {
+			val users = TableAlias(User)
+			val filter = userId match {
+				case Some(u) => User.fields.userId.equalsConstant(u)(users.name)
+				case None => Filter.noFilter
 			}
+			val qb = QueryBuilder
+				.from(users)
+				.where(filter)
+				.select(List(
+					User.fields.userId,
+					User.fields.userName,
+					User.fields.nameLast,
+					User.fields.nameFirst,
+					User.fields.email,
+					User.fields.locked,
+					User.fields.pwChangeRequired,
+					User.fields.active,
+					User.fields.hideFromClose
+				).map(_.alias(users)))
+			pb.executeQueryBuilder(qb).map(User.construct)
+		}
+		users.map(u => UserShape(
+			userId = u.values.userId.get,
+			userName = u.values.userName.get,
+			nameFirst = u.values.nameFirst.get,
+			nameLast = u.values.nameLast.get,
+			email = u.values.email.get,
+			locked = u.values.locked.get,
+			pwChangeRequired = u.values.pwChangeRequired.get,
+			active = u.values.active.get,
+			hideFromClose = u.values.hideFromClose.get
+		))
+	}
+
+	def getOne(userId: Int)(implicit PA: PermissionsAuthority): Action[AnyContent] = Action.async(req => {
+		val logger = PA.logger
+		PA.withRequestCache(StaffUserType, None, ParsedRequest(req), rc => {
+			val user = get(Some(userId), rc).head
 			implicit val format = UserShape.format
-			Future(Ok(Json.toJson(users.map(u => UserShape(
-				userId = u.values.userId.get,
-				userName = u.values.userName.get,
-				nameFirst = u.values.nameFirst.get,
-				nameLast = u.values.nameLast.get,
-				email = u.values.email.get,
-				locked = u.values.locked.get,
-				pwChangeRequired = u.values.pwChangeRequired.get,
-				active = u.values.active.get,
-				hideFromClose = u.values.hideFromClose.get
-			)))))
+			Future(Ok(Json.toJson(user)))
+		})
+	})
+
+	def getAll()(implicit PA: PermissionsAuthority): Action[AnyContent] = Action.async(req => {
+		val logger = PA.logger
+		PA.withRequestCache(StaffUserType, None, ParsedRequest(req), rc => {
+			val users = get(None, rc)
+			implicit val format = UserShape.format
+			Future(Ok(Json.toJson(users)))
 		})
 	})
 
@@ -64,7 +83,6 @@ class GetUsers @Inject()(implicit val exec: ExecutionContext) extends InjectedCo
 
 	object UserShape {
 		implicit val format = Json.format[UserShape]
-
 		def apply(v: JsValue): UserShape = v.as[UserShape]
 	}
 }
--- app/org/sailcbi/APIServer/Storable/Filter.scala
+++ app/org/sailcbi/APIServer/Storable/Filter.scala
@@ -7,5 +7,6 @@ case class Filter(preparedSQL: String, params: List[String]) {
 object Filter {
 	def and(fs: List[Filter]): Filter = Filter("( " + fs.map(_.preparedSQL).mkString(" AND ") + ") ", fs.flatMap(_.params))
 	def or(fs: List[Filter]): Filter = Filter("( " + fs.map(_.preparedSQL).mkString(" OR ") + ") ", fs.flatMap(_.params))
-	def empty: Filter = Filter("", List.empty)
+	val empty: Filter = Filter("", List.empty)
+	val noFilter: Filter = Filter("1=1", List.empty)
 }
