de16528f70f6a53e5a73dc676834a0b880be1aa2
Jonathan <coleji@bc.edu>
2020-12-12 14:38:17 -0500

    python route builder

---
+++ py-build-routes.py
@@ -0,0 +1,56 @@
+from os import listdir
+from os.path import isfile, join
+import re
+
+FILES_LOCATION = "conf/routes-build/src"
+OUTPUT_LOCATION = "conf/routes-build/dist/routes"
+CONF_FILE = "conf/private/server-properties"
+
+def getLevelFromServerProps():
+    """Search the server props file for the configured security level"""
+    pattern = r"RoutesSecurityLevel=(\d+)"
+    file = open(CONF_FILE, "r")
+    for line in file:
+        result = re.match(pattern, line)
+        if result:
+            return int(result.group(1))
+
+def getLevelFromFileName(fileName):
+    """Given a (path-less) filename, get the level from it"""
+    pattern = r'(\d+)\_.*'
+    result = re.match(pattern, fileName)
+    return int(result.group(1))
+
+def getFiles():
+    """Get all files in the FILES_LOCATION (file, fileWithPath, integer Level)"""
+    concatFullPath = lambda f : join(FILES_LOCATION, f)
+    excludeDirs = lambda f: isfile(join(FILES_LOCATION, f))
+    unsorted = [(f, concatFullPath(f), getLevelFromFileName(f)) for f in listdir(FILES_LOCATION) if excludeDirs(f)]
+    return sorted(unsorted, key=lambda t: t[2])
+
+def combineFiles(inPaths, outPath):
+    """Write all the inPaths to outPath"""
+    with open(outPath, 'w') as outfile:
+        for fname in inPaths:
+            with open(fname) as infile:
+                outfile.write(infile.read())
+
+def main():
+    level = getLevelFromServerProps()
+    print("Building routes with security level " + str(level))
+    pathsToKeep = []
+    pathsToDrop = []
+    for f in getFiles():
+        if (f[2] <= level):
+            pathsToKeep.append(f[1])
+        else :
+            pathsToDrop.append(f[1])
+    combineFiles(pathsToKeep, OUTPUT_LOCATION)
+    print("Building:")
+    for f in pathsToKeep:
+        print("*   " + f)
+    print("Ignoring:")
+    for f in pathsToDrop:
+        print("X   " + f)
+
+main()
