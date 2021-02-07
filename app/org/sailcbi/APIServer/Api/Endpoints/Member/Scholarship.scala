c3e5c20bf5f3d3c24ff651e7dcd9d0ba2dac2b6f
Jonathan Cole <coleji@bc.edu>
2021-02-06 20:20:07 -0500

    eii_mit use gen 2

--- app/org/sailcbi/APIServer/Api/Endpoints/Member/Scholarship.scala
+++ app/org/sailcbi/APIServer/Api/Endpoints/Member/Scholarship.scala
@@ -66,7 +66,7 @@ class Scholarship @Inject()(implicit exec: ExecutionContext) extends InjectedCon
 						   |from eii_mit
 						   |where adults = ?
 						   |and children = ?
-						   |and generation = 1
+						   |and generation = 2
 					 """.stripMargin
 
 					def mapResultSetRowToCaseObject(rs: ResultSetWrapper): Double = rs.getDouble(1)
