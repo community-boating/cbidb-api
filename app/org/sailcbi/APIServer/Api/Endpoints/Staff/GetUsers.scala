1bc0a6506686204bdac8dc1a984bac2f60c10b59
Jonathan Cole <coleji@bc.edu>
2021-06-15 00:20:14 -0400

    update user signature for api

--- app/org/sailcbi/APIServer/Api/Endpoints/Staff/GetUsers.scala
+++ app/org/sailcbi/APIServer/Api/Endpoints/Staff/GetUsers.scala
@@ -13,7 +13,7 @@ import javax.inject.Inject
 import scala.concurrent.{ExecutionContext, Future}
 
 class GetUsers @Inject()(implicit val exec: ExecutionContext) extends InjectedController {
-	private def get(userId: Option[Int], rc: StaffRequestCache): List[UserShape] = {
+	private def get(userId: Option[Int], rc: StaffRequestCache): List[User] = {
 		//implicit val rc: RequestCache = theRC
 		val u = User.getAuthedUser(rc)
 		if (u.values.userName.get != "JCOLE") {
@@ -42,18 +42,19 @@ class GetUsers @Inject()(implicit val exec: ExecutionContext) extends InjectedCo
 				).map(_.alias(users)))
 			rc.executeQueryBuilder(qb).map(User.construct)
 		}
-		users.map(u => UserShape(
-			userId = u.values.userId.get,
-			username = u.values.userName.get,
-			nameFirst = u.values.nameFirst.get,
-			nameLast = u.values.nameLast.get,
-			email = u.values.email.get,
-			locked = u.values.locked.get,
-			pwChangeRequired = u.values.pwChangeRequired.get,
-			active = u.values.active.get,
-			hideFromClose = u.values.hideFromClose.get,
-			userType = u.values.userType.get,
-		))
+		users
+//		users.map(u => UserShape(
+//			userId = u.values.userId.get,
+//			username = u.values.userName.get,
+//			nameFirst = u.values.nameFirst.get,
+//			nameLast = u.values.nameLast.get,
+//			email = u.values.email.get,
+//			locked = u.values.locked.get,
+//			pwChangeRequired = u.values.pwChangeRequired.get,
+//			active = u.values.active.get,
+//			hideFromClose = u.values.hideFromClose.get,
+//			userType = u.values.userType.get,
+//		))
 	}
 
 	def getOne(userId: Int)(implicit PA: PermissionsAuthority): Action[AnyContent] = Action.async(req => {
@@ -61,7 +62,7 @@ class GetUsers @Inject()(implicit val exec: ExecutionContext) extends InjectedCo
 		PA.withRequestCache(StaffRequestCache)(None, ParsedRequest(req), rc => {
 			get(Some(userId), rc) match {
 				case l :: _ => {
-					implicit val format = UserShape.format
+			//		implicit val format = UserShape.format
 					Future(Ok(Json.toJson(l)))
 				}
 				case _ => Future(Ok(ValidationResult.from("No user found").toResultError.asJsObject()))
@@ -74,26 +75,26 @@ class GetUsers @Inject()(implicit val exec: ExecutionContext) extends InjectedCo
 		val logger = PA.logger
 		PA.withRequestCache(StaffRequestCache)(None, ParsedRequest(req), rc => {
 			val users = get(None, rc)
-			implicit val format = UserShape.format
+	//		implicit val format = UserShape.format
 			Future(Ok(Json.toJson(users)))
 		})
 	})
 
-	case class UserShape (
-		userId: Int,
-		username: String,
-		nameFirst: Option[String],
-		nameLast: Option[String],
-		email: String,
-		locked: Boolean,
-		pwChangeRequired: Boolean,
-		active: Boolean,
-		hideFromClose: Boolean,
-		userType: Option[String],
-	)
-
-	object UserShape {
-		implicit val format = Json.format[UserShape]
-		def apply(v: JsValue): UserShape = v.as[UserShape]
-	}
+//	case class UserShape (
+//		userId: Int,
+//		username: String,
+//		nameFirst: Option[String],
+//		nameLast: Option[String],
+//		email: String,
+//		locked: Boolean,
+//		pwChangeRequired: Boolean,
+//		active: Boolean,
+//		hideFromClose: Boolean,
+//		userType: Option[String],
+//	)
+//
+//	object UserShape {
+//		implicit val format = Json.format[UserShape]
+//		def apply(v: JsValue): UserShape = v.as[UserShape]
+//	}
 }
