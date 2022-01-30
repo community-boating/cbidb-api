8e6f5228b5bf87f0bbb2d02c8a33325be6ed8b3b
Jonathan Cole <jon@jcole.io>
2022-01-30 13:44:26 -0500

    independent props for pool size of main vs temp

--- app/com/coleji/neptune/Core/OracleDatabaseConnection.scala
+++ app/com/coleji/neptune/Core/OracleDatabaseConnection.scala
@@ -19,10 +19,11 @@ object OracleDatabaseConnection {
 		val password = pw.getString("password")
 		val tempUsername = pw.getString("temptableusername")
 		val tempPassword = pw.getString("temptablepassword")
-		val poolSize = pw.getOptionalString("maxPoolSize").map(_.toInt)
+		val poolSize = pw.getOptionalString("maxPoolSize").map(_.toInt).getOrElse(2)
+		val poolSizeTemp = pw.getOptionalString("maxPoolSizeTemp").map(_.toInt).getOrElse(1)
 
 		val mainConfig = getDataSourceConfig(host, port, sid, username, password, poolSize)
-		val tempConfig = getDataSourceConfig(host, port, sid, tempUsername, tempPassword, poolSize)
+		val tempConfig = getDataSourceConfig(host, port, sid, tempUsername, tempPassword, poolSizeTemp)
 
 		new DatabaseGateway(
 			mainPool = new ConnectionPoolWrapper(new HikariDataSource(mainConfig)),
@@ -33,16 +34,15 @@ object OracleDatabaseConnection {
 		)
 	}
 
-	private def getDataSourceConfig(host: String, port: String, sid: String, username: String, password: String, poolSize: Option[Int]): HikariConfig = {
+	private def getDataSourceConfig(host: String, port: String, sid: String, username: String, password: String, poolSize: Int): HikariConfig = {
 		val config = new HikariConfig()
-		val poolSizeToUse = poolSize.getOrElse(2)
-		println("max pool size: " + poolSizeToUse)
+		println("username: " + username + " max pool size: " + poolSize)
 		config.setJdbcUrl(s"jdbc:oracle:thin:@$host:$port:$sid")
 		config.setUsername(username)
 		config.setPassword(password)
 
 		// other options
-		config.setMaximumPoolSize(poolSizeToUse)
+		config.setMaximumPoolSize(poolSize)
 		config.setLeakDetectionThreshold(30 * 1000)
 
 		config
