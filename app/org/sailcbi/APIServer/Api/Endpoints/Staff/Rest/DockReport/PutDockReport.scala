01c8c904b65682d6090fa7c5f7189a4eb24e2e21
Jonathan Cole <jon@jcole.io>
2022-05-31 19:42:06 -0400

    remove permission from putdockreport

--- app/org/sailcbi/APIServer/Api/Endpoints/Staff/Rest/DockReport/PutDockReport.scala
+++ app/org/sailcbi/APIServer/Api/Endpoints/Staff/Rest/DockReport/PutDockReport.scala
@@ -16,7 +16,7 @@ class PutDockReport @Inject()(implicit exec: ExecutionContext) extends RestContr
 	def post()(implicit PA: PermissionsAuthority) = Action.async { request =>
 		val parsedRequest = ParsedRequest(request)
 		PA.withParsedPostBodyJSON(parsedRequest.postJSON, PutDockReportDto.apply)(parsed => {
-			PA.withRequestCache(StaffRequestCache, CbiAccess.permissions.PERM_GENERAL_ADMIN)(None, parsedRequest, rc => {
+			PA.withRequestCache(StaffRequestCache)(None, parsedRequest, rc => {
 				put(rc, parsed) match {
 					case Left(ve: ValidationError) => Future(Ok(ve.toResultError.asJsObject()))
 					case Right(i: DockReport) => Future(Ok(Json.toJson(PutDockReportDto.applyWithSubObjects(rc)(i))))
