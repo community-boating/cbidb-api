a30625c5ab2579bd0ffbbed25eb5121ed9de459d
Jonathan Cole <jon@jcole.io>
2022-07-04 10:40:15 -0400

    remove hardcoded rcpt email from ssmtp emailer

--- app/com/coleji/neptune/Core/Emailer/SSMTPEmailer.scala
+++ app/com/coleji/neptune/Core/Emailer/SSMTPEmailer.scala
@@ -6,12 +6,12 @@ class SSMTPEmailer private[Core](alwaysSendTo: Option[String]) extends Emailer {
 	private def sanitize(s: String): String = s.replace("\"", "\\\"").replace("'", "\\'")
 
 	private[Core] def send(subject: String, body: String, to: String = "jon@community-boating.org"): Unit = {
-		val sendTo = alwaysSendTo match {
-			case Some(s) => s
-			case None => to
-		}
+//		val sendTo = alwaysSendTo match {
+//			case Some(s) => s
+//			case None => to
+//		}
 
-		val command = "ssmtp " + sendTo + " -F CBI-API"
+		val command = "ssmtp " + to + " -F CBI-API"
 		val stdin = "Subject: " + subject + "\n\n" + body
 		println(command)
 		ShellManager.execute(command, None, Some(20000), None, Some(stdin))
