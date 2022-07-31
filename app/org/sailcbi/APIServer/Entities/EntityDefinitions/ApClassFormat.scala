c66e3e5b2eb69e6e3be7455588ef5bfc9393c4fd
Jonathan Cole <jon@jcole.io>
2022-07-30 21:19:04 -0400

    entities for ap class type and format

--- app/org/sailcbi/APIServer/Entities/EntityDefinitions/ApClassFormat.scala
+++ app/org/sailcbi/APIServer/Entities/EntityDefinitions/ApClassFormat.scala
@@ -1,7 +1,7 @@
 package org.sailcbi.APIServer.Entities.EntityDefinitions
 
-import com.coleji.neptune.Storable.FieldValues.{IntFieldValue, NullableStringFieldValue}
-import com.coleji.neptune.Storable.Fields.{IntDatabaseField, NullableStringDatabaseField}
+import com.coleji.neptune.Storable.FieldValues.{DoubleFieldValue, IntFieldValue, NullableDoubleFieldValue, NullableIntFieldValue, NullableStringFieldValue}
+import com.coleji.neptune.Storable.Fields.{DoubleDatabaseField, IntDatabaseField, NullableDoubleDatabaseField, NullableIntDatabaseField, NullableStringDatabaseField}
 import com.coleji.neptune.Storable._
 import com.coleji.neptune.Util.Initializable
 
@@ -14,6 +14,11 @@ class ApClassFormat extends StorableClass(ApClassFormat) {
 		val formatId = new IntFieldValue(self, ApClassFormat.fields.formatId)
 		val typeId = new IntFieldValue(self, ApClassFormat.fields.typeId)
 		val description = new NullableStringFieldValue(self, ApClassFormat.fields.description)
+		val signupMinDefaultOverride = new NullableIntFieldValue(self, ApClassFormat.fields.signupMinDefaultOverride)
+		val signupMaxDefaultOverride = new NullableIntFieldValue(self, ApClassFormat.fields.signupMaxDefaultOverride)
+		val sessionCtDefault = new IntFieldValue(self, ApClassFormat.fields.sessionCtDefault)
+		val sessionLengthDefault = new DoubleFieldValue(self, ApClassFormat.fields.sessionLengthDefault)
+		val priceDefaultOverride = new NullableDoubleFieldValue(self, ApClassFormat.fields.priceDefaultOverride)
 	}
 }
 
@@ -23,7 +28,12 @@ object ApClassFormat extends StorableObject[ApClassFormat] {
 	object fields extends FieldsObject {
 		val formatId = new IntDatabaseField(self, "FORMAT_ID")
 		val typeId = new IntDatabaseField(self, "TYPE_ID")
-		val description = new NullableStringDatabaseField(self, "DESCRIPTION", 100)
+		val description = new NullableStringDatabaseField(self, "DESCRIPTION", 1000)
+		val signupMinDefaultOverride = new NullableIntDatabaseField(self, "SIGNUP_MIN_DEFAULT_OVERRIDE")
+		val signupMaxDefaultOverride = new NullableIntDatabaseField(self, "SIGNUP_MAX_DEFAULT_OVERRIDE")
+		val sessionCtDefault = new IntDatabaseField(self, "SESSION_CT_DEFAULT")
+		val sessionLengthDefault = new DoubleDatabaseField(self, "SESSION_LENGTH_DEFAULT")
+		val priceDefaultOverride = new NullableDoubleDatabaseField(self, "PRICE_DEFAULT_OVERRIDE")
 	}
 
 	def primaryKey: IntDatabaseField = fields.formatId
--- app/org/sailcbi/APIServer/Entities/EntityDefinitions/ApClassType.scala
+++ app/org/sailcbi/APIServer/Entities/EntityDefinitions/ApClassType.scala
@@ -1,14 +1,25 @@
 package org.sailcbi.APIServer.Entities.EntityDefinitions
 
-import com.coleji.neptune.Storable.FieldValues.{IntFieldValue, StringFieldValue}
-import com.coleji.neptune.Storable.Fields.{IntDatabaseField, StringDatabaseField}
+import com.coleji.neptune.Storable.FieldValues.{BooleanFieldValue, IntFieldValue, NullableDoubleFieldValue, NullableIntFieldValue, NullableStringFieldValue, StringFieldValue}
+import com.coleji.neptune.Storable.Fields.{BooleanDatabaseField, IntDatabaseField, NullableDoubleDatabaseField, NullableIntDatabaseField, NullableStringDatabaseField, StringDatabaseField}
 import com.coleji.neptune.Storable._
 
 class ApClassType extends StorableClass(ApClassType) {
 	object values extends ValuesObject {
 		val typeId = new IntFieldValue(self, ApClassType.fields.typeId)
 		val typeName = new StringFieldValue(self, ApClassType.fields.typeName)
+		val ratingPrereq = new NullableIntFieldValue(self,  ApClassType.fields.ratingPrereq)
+		val classPrereq = new NullableIntFieldValue(self,  ApClassType.fields.classPrereq)
+		val ratingOverkill = new NullableIntFieldValue(self,  ApClassType.fields.ratingOverkill)
 		val displayOrder = new IntFieldValue(self, ApClassType.fields.displayOrder)
+		val descShort = new NullableStringFieldValue(self,  ApClassType.fields.descShort)
+		val descLong = new NullableStringFieldValue(self,  ApClassType.fields.descLong)
+		val classOverkill = new NullableIntFieldValue(self,  ApClassType.fields.classOverkill)
+		val noSignup = new BooleanFieldValue(self,  ApClassType.fields.noSignup)
+		val priceDefault = new NullableDoubleFieldValue(self, ApClassType.fields.priceDefault)
+		val signupMinDefault = new NullableIntFieldValue(self,  ApClassType.fields.signupMinDefault)
+		val signupMaxDefault = new NullableIntFieldValue(self,  ApClassType.fields.signupMaxDefault)
+		val disallowIfOverkill = new BooleanFieldValue(self,  ApClassType.fields.disallowIfOverkill)
 	}
 }
 
@@ -17,8 +28,19 @@ object ApClassType extends StorableObject[ApClassType] {
 
 	object fields extends FieldsObject {
 		val typeId = new IntDatabaseField(self, "TYPE_ID")
-		val typeName = new StringDatabaseField(self, "TYPE_NAME", 50)
+		val typeName = new StringDatabaseField(self, "TYPE_NAME", 200)
+		val ratingPrereq = new NullableIntDatabaseField(self, "RATING_PREREQ")
+		val classPrereq = new NullableIntDatabaseField(self, "CLASS_PREREQ")
+		val ratingOverkill = new NullableIntDatabaseField(self, "RATING_OVERKILL")
 		val displayOrder = new IntDatabaseField(self, "DISPLAY_ORDER")
+		val descShort = new NullableStringDatabaseField(self, "DESC_SHORT", 4000)
+		val descLong = new NullableStringDatabaseField(self, "DESC_LONG", 4000)
+		val classOverkill = new NullableIntDatabaseField(self, "CLASS_OVERKILL")
+		val noSignup = new BooleanDatabaseField(self, "NO_SIGNUP", true)
+		val priceDefault = new NullableDoubleDatabaseField(self, "PRICE_DEFAULT")
+		val signupMinDefault = new NullableIntDatabaseField(self, "SIGNUP_MIN_DEFAULT")
+		val signupMaxDefault = new NullableIntDatabaseField(self, "SIGNUP_MAX_DEFAULT")
+		val disallowIfOverkill = new BooleanDatabaseField(self, "DISALLOW_IF_OVERKILL", true)
 	}
 
 	def primaryKey: IntDatabaseField = fields.typeId
