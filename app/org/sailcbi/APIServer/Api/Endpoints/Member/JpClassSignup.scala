00917237df071429b42dc36932d46df897ab7d54
Jonathan Cole <jcole@newt.steingard.lan>
2023-04-02 10:31:26 -0400

    fix merge typo

--- app/org/sailcbi/APIServer/Api/Endpoints/Member/JpClassSignup.scala
+++ app/org/sailcbi/APIServer/Api/Endpoints/Member/JpClassSignup.scala
@@ -73,7 +73,7 @@ class JpClassSignup @Inject()(implicit exec: ExecutionContext) extends InjectedC
 						val signupId = PortalLogic.actuallyEnroll(rc, parsed.instanceId, parsed.juniorId, None, doEnroll = doEnroll, fullEnroll = true, None).orNull
 						Future(Ok(new JsObject(Map("signupId" -> JsNumber(signupId.toInt)))))
 					}
-					case Left(e: ValidationError) => Future(Ok(e.toResultError.asJsObject()))
+					case Left(e: ValidationError) => Future(Ok(e.toResultError.asJsObject))
 				}
 			})
 		})
