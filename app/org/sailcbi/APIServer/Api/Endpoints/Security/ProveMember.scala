a31acac35a6439406df0b7b26a84fac78ce1b045
Alex Banks <alexb@community-boatingorg>
2025-04-09 16:26:46 -0400

    fix for member auth change

--- app/org/sailcbi/APIServer/Api/Endpoints/Security/ProveMember.scala
+++ app/org/sailcbi/APIServer/Api/Endpoints/Security/ProveMember.scala
@@ -3,6 +3,7 @@ package org.sailcbi.APIServer.Api.Endpoints.Security
 import com.coleji.neptune.Core.{ParsedRequest, PermissionsAuthority}
 import com.coleji.neptune.IO.PreparedQueries.{PreparedQueryForSelect, PreparedQueryForUpdateOrDelete, PreparedValue}
 import com.coleji.neptune.Storable.ResultSetWrapper
+import org.sailcbi.APIServer.Entities.MagicIds
 import org.sailcbi.APIServer.IO.Portal.PortalLogic
 import org.sailcbi.APIServer.UserTypes.{BouncerRequestCache, MemberRequestCache, ProtoPersonRequestCache}
 import play.api.libs.json.{JsValue, Json}
@@ -24,6 +25,7 @@ class ProveMember @Inject()(implicit exec: ExecutionContext) extends InjectedCon
 				println("Authed person: " + protoRC.getAuthedPersonId)
 				println("Member RC Username: " + memberRC.userName)
 				println("Authed Member Person Id: " + authedPersonId)
+
 				val personQ = new PreparedQueryForSelect[(Option[String], Option[String], Option[String])](Set(MemberRequestCache)) {
 					override def mapResultSetRowToCaseObject(rsw: ResultSetWrapper): (Option[String], Option[String], Option[String]) =
 						(rsw.getOptionString(1), rsw.getOptionString(2), rsw.getOptionString(3))
@@ -38,6 +40,24 @@ class ProveMember @Inject()(implicit exec: ExecutionContext) extends InjectedCon
 				}
 				val (nameFirst, nameLast, email) = memberRC.executePreparedQueryForSelect(personQ).head
 				val protoPersonId = PortalLogic.persistStandalonePurchaser(protoRC, protoRC.userName, protoRC.getAuthedPersonId, nameFirst, nameLast, email, authedAsOverride = Some(authedPersonId))
+				val orderIdDonateMember = PortalLogic.getOrderId(memberRC, memberRC.getAuthedPersonId, MagicIds.ORDER_NUMBER_APP_ALIAS.DONATE)
+				val orderIdDonateProto = PortalLogic.getOrderId(protoRC, protoPersonId, MagicIds.ORDER_NUMBER_APP_ALIAS.DONATE)
+				val orderIdGCMember = PortalLogic.getOrderId(memberRC, memberRC.getAuthedPersonId, MagicIds.ORDER_NUMBER_APP_ALIAS.DONATE)
+				val orderIdGCProto = PortalLogic.getOrderId(protoRC, protoPersonId, MagicIds.ORDER_NUMBER_APP_ALIAS.DONATE)
+
+				val updateSCDonationsQ = new PreparedQueryForUpdateOrDelete(Set(MemberRequestCache)) {
+					override val params: List[String] = List(
+						orderIdDonateMember.toString,
+					)
+
+					override def getQuery: String =
+						s"""
+							|update shopping_cart_donations
+							|set order_id = ?
+							|where orderId = $orderIdDonateProto
+							|""".stripMargin
+				}
+
 				val updateProtoQ = new PreparedQueryForUpdateOrDelete(Set(MemberRequestCache)) {
 					override val params: List[String] = List(
 						authedPersonId.toString,
